d478b823d0d56e001d92f3a1f46baa75b57c8ba3
Taylor Creekbaum <taylorcreekbaum@gmail.com>
2017-12-16 17:48:11 -0600

    project 3

---
+++ prj3/Love-o-Meter/Love-o-Meter.ino
@@ -0,0 +1,48 @@
+const int sensorPin = A0;
+const float baselineTemp = 24.7;
+
+void setup(){
+    Serial.begin(9600); // open a serial port
+    for(int pinNumber = 2; pinNumber < 5; pinNumber++){
+        pinMode(pinNumber, OUTPUT);
+        digitalWrite(pinNumber, LOW);
+    }
+}
+
+void loop(){
+    // get the raw ADC value from the microcontroller
+    int sensorVal = analogRead(sensorPin);
+
+    // convert the ADC reading to voltage
+    float voltage = (sensorVal / 1024.0) * 5.0;
+
+    // convert the voltage to temperature in degrees
+    float temperature = (voltage - 0.5) * 100;
+
+    // Print the data
+    Serial.print("Sensor Value: ");
+    Serial.print(sensorVal);
+    Serial.print(", Volts: ");
+    Serial.print(voltage);
+    Serial.print(" , degrees C: ");
+    Serial.println(temperature);
+
+    if(temperature < baselineTemp){
+        digitalWrite(2, LOW);
+        digitalWrite(3, LOW);
+        digitalWrite(4, LOW);
+    } else if (temperature >= baselineTemp + 2 && temperature < baselineTemp + 4){
+        digitalWrite(2, HIGH);
+        digitalWrite(3, LOW);
+        digitalWrite(4, LOW);
+    } else if (temperature >= baselineTemp + 4 && temperature < baselineTemp + 6){
+        digitalWrite(2, HIGH);
+        digitalWrite(3, HIGH);
+        digitalWrite(4, LOW);
+    } else if (temperature >= baselineTemp + 6){
+        digitalWrite(2, HIGH);
+        digitalWrite(3, HIGH);
+        digitalWrite(4, HIGH);
+    }
+    delay(1);
+}
